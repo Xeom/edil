92e790e6ab2807209d66b0d38a0b74600ff07b2c
Xeom <fwharf.fw@gmail.com>
2018-02-06 22:26:49 +0000

    Removed input functions from cli.c

--- cli.c
+++ cli.c
@@ -1,10 +1,16 @@
+#ifndef _POSIX_C_SOURCE
+# define _POSIX_C_SOURCE 1
+#endif
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <sys/ioctl.h>
 
+#include "inp.h"
 #include "cmd.h"
 #include "text.h"
 
@@ -16,154 +22,9 @@
 #define CLI_COL_BLANK text_col_black|text_col_bold
 #define CLI_STR_BLANK "\xc2\xbb"
 
-#define CLI_INP_TMOUT {.tv_sec = 0, .tv_usec = 50000}
-
-int cli_h, cli_w;
+int cli_h = 16, cli_w = 64;
 int cli_alive = 0;
 static struct termios cli_torig;
-vec cli_keycodes;
-
-cli_keycode cli_keycodes_static[] =
-{
-    {cli_key_up,     "[A"},
-    {cli_key_down,   "[B"},
-    {cli_key_right,  "[C"},
-    {cli_key_left,   "[D"},
-    {cli_key_home,   "[1~"},
-    {cli_key_del,    "[3~"},
-    {cli_key_end,    "[4~"},
-    {cli_key_pgup,   "[5~"},
-    {cli_key_pgdn,   "[6~"},
-    {cli_key_insert, "[4h"},
-    {cli_key_f1,     "OP"},
-    {cli_key_f2,     "OQ"},
-    {cli_key_f3,     "OR"},
-    {cli_key_f4,     "OS"},
-    {cli_key_f5,     "[15~"},
-    {cli_key_f6,     "[17~"},
-    {cli_key_f7,     "[18~"},
-    {cli_key_f8,     "[19~"},
-    {cli_key_f9,     "[20~"},
-    {cli_key_f10,    "[21~"},
-    {cli_key_f11,    "[23~"},
-    {cli_key_f12,    "[24~"}
-};
-
-static int cli_keycode_cmp(const void *a, const void *b)
-{
-    return strcmp(((const cli_keycode *)a)->code, ((const cli_keycode *)b)->code);
-}
-
-void cli_init(void)
-{
-    size_t numkeys;
-    struct termios tinfo;
-    int    flags;
-    
-    tcgetattr(STDIN_FILENO, &tinfo);
-
-    memcpy(&cli_torig, &tinfo, sizeof(struct termios));
-
-    tinfo.c_lflag -= tinfo.c_lflag & ICANON;
-    tinfo.c_lflag -= tinfo.c_lflag & ECHO;
-
-    tinfo.c_cc[VMIN]  = 1;
-    tinfo.c_cc[VTIME] = 0;
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &tinfo);
-
-//   cli_resize();
-
-    fputs("\033[2J\033[?25l", stdout);
-
-    numkeys = sizeof(cli_keycodes_static) / sizeof(cli_keycode);
- 
-    qsort(&cli_keycodes_static, numkeys, sizeof(cli_keycode), cli_keycode_cmp);
- 
-    vec_init(&cli_keycodes, sizeof(cli_keycode));
-    vec_ins(&cli_keycodes, 0, numkeys, &cli_keycodes_static);
-    flags = fcntl(STDIN_FILENO, F_GETFD);
-    fcntl(STDIN_FILENO, F_SETFD, flags | O_NONBLOCK);
-
-
-    cli_alive = 1;
-}
-
-void cli_kill(void)
-{
-    tcsetattr(STDIN_FILENO, TCSANOW, &cli_torig);
-
-    fputs("\033[2J\033[?25h", stdout);
-
-    vec_kill(&cli_keycodes);
-}
-
-static cli_key cli_get_escaped_key(char chr)
-{
-    static char code[16];
-    static int  code_ind   = 0;
-    static int  search_ind = 0;
-    static int  rtn;
-
-    code[code_ind] = chr;
-    code[code_ind + 1] = '\0';
-    code_ind += 1;
-
-    while (code_ind <= 14)
-    {
-        int cmp;
-        cli_keycode *curr;
-
-        curr = vec_get(&cli_keycodes, search_ind);
-        if (curr) cmp = strcmp(code, curr->code);
-
-        if (cmp == 0) {rtn = curr->key;             break;}
-        if (!curr)    {rtn = cli_key_esc | code[0]; break;}
-        if (cmp >  0) search_ind++;
-        if (cmp <  0)
-        {
-            if (memcmp(code, curr->code, code_ind) == 0)
-                return 0;
-            
-            rtn = cli_key_esc | code[0];
-            break;
-        }
-    }
-
-    code_ind   = 0;
-    search_ind = 0;
-
-    return rtn;
-}
-
-cli_key cli_get_key(char chr)
-{
-    static int escaped = 0;
-    cli_key    rtn;
-
-    if (escaped)
-    {    
-        rtn = cli_get_escaped_key(chr);
-        if (rtn == 0) return cli_key_none;
-    }
-    else if (chr == '\033')
-    {
-        escaped = 1;
-        return cli_key_none;
-    }
-    else
-        rtn = chr;
-
-    escaped = 0;
-
-    if ((rtn & ~cli_key_esc) < 32)
-    {
-        rtn += 0x40;
-        rtn |= cli_key_ctrl;
-    }
-
-    return rtn;
-}
 
 void cli_goto(size_t cn, size_t ln)
 {
@@ -194,65 +55,83 @@ void cli_fg(text_col col)
 
 void cli_line_blank(void)
 {
+    fputs("\033[K", stdout);
     cli_fg(CLI_COL_BLANK);
     fputs(CLI_STR_BLANK, stdout);
     cli_fg(text_col_none);
 }
 
-void cli_line_here(text_buf *b, size_t cn, size_t ln, size_t len)
+void cli_chrs_here(vec *chrs, size_t len)
 {
-    text_char space = {.utf8 = " ", .fg = text_col_none};
-    text_col nextcol, col = -1;
-    vec line;
-    int looping;
+    text_col prevcol = -1;
+    text_col col;
+    size_t ind;
+
+    if (len == 0) return;
 
     fputs("\033[K", stdout);
 
-    if (!(b->flags & text_flag_vis)) return;
-    if (ln >= text_buf_len(b))       return;
-
-    text_buf_get(b, ln, &line);
-
-    looping = 1;
-
-    while (looping)
-      {
+    for (ind = 0; ind < vec_len(chrs); ind++)
+    {
         text_char *chr;
 
-        chr = vec_get(&line, cn);
-        if (chr == NULL) 
-        {
-            chr = &space;
-            looping = 0;
-        }
+        chr = vec_get(chrs, ind);
+        if (--len == 0) break;
 
-        if (--len == 0)  break;
+        col = chr->fg;
+        if (col != prevcol) cli_fg(col);
 
-        nextcol = chr->fg;
- 
-        if (cn == b->cur.cn1 && ln == b->cur.ln1)
-            nextcol ^= CLI_COL_CUR1;
-
-        else if (cn == b->cur.cn2 && ln == b->cur.ln2)
-            nextcol ^= CLI_COL_CUR2;
-
-        if (col != nextcol)
-            cli_fg(nextcol);
-
-        col = nextcol;
         fwrite(chr->utf8, 1, text_utf8_len(chr->utf8[0]), stdout);
 
-        cn += 1;
+        prevcol = col;
     }
-    
-    vec_kill(&line);
+
     cli_fg(text_col_none);
+}  
+
+void cli_line_here(text_buf *b, size_t cn, size_t ln, size_t len)
+{
+    text_char space = {.utf8 = " ", .fg = text_col_none};
+    vec      line;
+    text_cur cur;
+    size_t   linelen;
+
+    if (!(text_buf_getflag(b) & text_flag_vis)) return;
+    if (ln > text_buf_len(b))                   return;
+
+    text_buf_get(b, ln, &line);
+    text_buf_getcur(b,  &cur);
+
+    linelen = vec_len(&line);
+
+    if (cur.ln1 == ln)
+    {
+        text_char *curchr;
+        if (cur.cn1 == linelen) vec_ins(&line, linelen, 1, &space);
+
+        curchr = vec_get(&line, cur.cn1);
+        if (curchr) curchr->fg ^= CLI_COL_CUR1;
+    }
+    if (cur.ln2 == ln && !(cur.cn1 == cur.cn2 && cur.ln1 == cur.ln2))
+    {
+        text_char *curchr;
+        if (cur.cn2 == linelen) vec_ins(&line, linelen, 1, &space);
+
+        curchr = vec_get(&line, cur.cn2);
+        if (curchr) curchr->fg ^= CLI_COL_CUR2;
+    }
+
+    if (cn) vec_del(&line, 0, cn);
+
+    cli_chrs_here(&line, len);
+
+    vec_kill(&line);
 }
 
 void cli_line(text_buf *b, size_t cn, size_t ln)
 {
-    if (ln    < b->scrolly || ln > b->scrolly + b->h) return;
-    if                       (cn > b->scrollx + b->w) return;
+    if (ln < b->scrolly || ln >= b->scrolly + b->h) return;
+    if                    (cn >= b->scrollx + b->w) return;
 
     if (cn < b->scrollx) cn = b->scrollx;
 
@@ -277,64 +156,67 @@ void cli_lines_after(text_buf *b, size_t ln)
         cli_line(b, 0, ln++);
 }
 
-pthread_t cli_input_thread;
+int cli_needs_resize = 0;
 
-pthread_mutex_t cli_input_mtx;
-pthread_cond_t  cli_input_cond;
-pthread_mutex_t cli_input_cond_mtx;
-
-vec cli_input_vec;
-
-void *cli_input_listen(void *arg)
+void cli_handle_winch(int sig)
 {
-    char   chr;
-    vec   *v;
-    size_t len;
-
-    v = &cli_input_vec;
-
-    while (1)
-    {
-        chr = getchar();
-
-        pthread_mutex_lock(&cli_input_mtx);
-
-        len = vec_len(v);
-        vec_ins(v, len, 1, &chr);
-
-        pthread_mutex_unlock(&cli_input_mtx);
-        pthread_cond_signal(&cli_input_cond);
-    }
+    cli_needs_resize = 1;
 }
 
-void cli_input(void)
-{   
-    char   chr;
-    vec   *v;
-    size_t len;
-    cli_key key;
+void cli_resize(void)
+{
+    struct winsize w;
 
-    v = &cli_input_vec;
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &w);
+    cli_h = w.ws_row;
+    cli_w = w.ws_col;
+}
+
+
+void cli_init(void)
+{
+    struct termios tinfo;
+    struct sigaction act;
+
+    /* Set terminal attributes */   
+    tcgetattr(STDIN_FILENO, &tinfo);
+
+    memcpy(&cli_torig, &tinfo, sizeof(struct termios));
+
+    tinfo.c_lflag -= tinfo.c_lflag & ICANON;
+    tinfo.c_lflag -= tinfo.c_lflag & ECHO;
+
+    tinfo.c_cc[VMIN]  = 1;
+    tinfo.c_cc[VTIME] = 0;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &tinfo);
     
-    pthread_mutex_lock(&cli_input_mtx);
-    pthread_cond_wait(&cli_input_cond, &cli_input_mtx);
-    pthread_mutex_unlock(&cli_input_mtx);    
+    /* Get window size */
+    cli_resize();
 
-    do
-    {
-        pthread_mutex_lock(&cli_input_mtx);
-        chr = *((char *)vec_get(v, 0));
-        vec_del(v, 0, 1);
-        len = vec_len(v);
-        pthread_mutex_unlock(&cli_input_mtx);
+    /* Clear sreen and hide cursor */
+    fputs("\033[2J\033[?25l", stdout);
 
-        key = cli_get_key(chr);
-        cmd_handle_key(key);
-    } while (len);
+    /* Mount window size handler */
+    act.sa_handler = cli_handle_winch;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
 
-    cmd_ins_flush();
+    sigaction(SIGWINCH, &act, NULL);
 
-    fflush(stdout);        
+    /* It's alive! */
+    cli_alive = 1;
+
+
+}
+
+void cli_kill(void)
+{
+    /* Reset terminal attributes */
+    tcsetattr(STDIN_FILENO, TCSANOW, &cli_torig);
+
+    /* Clear screen and show cursor */
+    fputs("\033[2J\033[?25h", stdout);
 }
 
 int main(void)
@@ -345,19 +227,24 @@ int main(void)
     b.flags |= text_flag_vis;
     text_cur_buf = &b;
 
-    vec_init(&cli_input_vec, 1);
-
     cmd_init();
-    cli_init();
+    cli_init(); 
+    inp_init();
+
     cli_lines_after(&b, 0);
 
     fflush(stdout);
 
-    pthread_mutex_init(&cli_input_mtx, NULL);
-    pthread_create(&cli_input_thread, NULL, cli_input_listen, NULL);
-
-    while (cli_alive) cli_input();
+    while (cli_alive) 
+    {
+        if (cli_needs_resize)
+            cli_resize();
+        else
+            inp_loop();
+    }
     cli_kill();
+    inp_kill();
+    cmd_kill();
 
     return 0;
 }
