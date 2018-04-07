37295bd1a0be653f8a972aebe666e57a6675506b
Xeom <fwharf.fw@gmail.com>
2018-04-07 21:56:34 +0100

    Added --cmd option
     * --cmd option allows commands on startup
     * [file] is now availible too

--- src/edil.c
+++ src/edil.c
@@ -44,10 +44,17 @@ static char *welcome = "\n"
 static char *help = "\n"
 "Edil - My text editor. v" STRIFY(VERSION) "\n\n"
 
-"More complete documentation can be found in the README.md \n"
-"file, available in my repo at http://github.com/Xeom/edil.\n\n"
+"More complete documentation can be found in the README.md\n"
+"file, and in the doc/ directory of my repository."
+"This should be availible at http://github.com/Xeom/edil.\n\n"
+
+"Usage: edil [options ...] [files ...]\n\n"
 
 "Options:\n"
+"    --cmd      -c    Specify a command to run on startup.\n"
+"                       e.g. edil --cmd \"goto 1 1\"\n"
+"                     will move the cursor to the start of the\n"
+"                     file when edil starts up.\n"
 "    --help     -h    Display this help message.\n"
 "    --version  -v    Show the current version of edil, and\n"
 "                     its compilation time\n";
@@ -55,15 +62,17 @@ static char *help = "\n"
 static char *version =
 "Edil v" STRIFY(VERSION) ", -- Compiled (" STRIFY(COMPILETIME) ")\n";
 
-static char *argerror =
-"Unknown argument.\n"
-"Try 'edil --help' for usage information.\n";
+#define argerror(error) #error ".\n" \
+    "Try 'edil --help' for usage information.\n"
 
 static void load_string(win *w, char *str);
 static void init_all(void);
 static void kill_all(void);
 static void loop(void);
 static void process_arg(int argc, char **argv, int *n);
+
+static vec *new_startup_cmd(void);
+static void run_startup_cmd(void);
 
 #define argis(a) (strcmp(argv[*n], #a) == 0)
 
@@ -79,13 +88,59 @@ static void process_arg(int argc, char **argv, int *n)
         fputs(version, stdout);
         exit(0);
     }
+    else if (argis(--cmd) || argis(-c))
+    {
+        vec *cmd;
+        *n += 1;
+        if (*n >= argc)
+        {
+            fputs(argerror('--cmd' needs an argument), stdout);
+            exit(0);
+        }
+
+        cmd = new_startup_cmd();
+        chr_from_str(cmd, argv[*n]);
+    }
+    else if (strncmp(argv[*n], "--", 2) == 0)
+    {
+        fputs(argerror(Unknown argument), stdout);
+        exit(0);
+    }
     else
     {
-        fputs(argerror, stdout);
-        exit(0);
+        vec *cmd;
+        cmd = new_startup_cmd();
+        chr_format(cmd, "new \"%s\"", argv[*n]);
     }
 
     *n += 1;
+}
+
+vec startup_cmd;
+
+void run_startup_cmd(void)
+{
+    size_t ind, len;
+
+    len = vec_len(&startup_cmd);
+    for (ind = 0; ind < len; ++ind)
+    {
+        vec *cmd;
+        cmd = vec_get(&startup_cmd, ind);
+        ui_cmd_cb(win_cur, cmd);
+        vec_kill(cmd);
+    }
+    
+    vec_kill(&startup_cmd);
+}
+
+vec *new_startup_cmd(void)
+{
+    vec *cmd;
+    cmd = vec_ins(&startup_cmd, vec_len(&startup_cmd), 1, NULL);
+    vec_init(cmd, sizeof(chr));
+
+    return cmd;    
 }
 
 static void init_all(void)
@@ -161,6 +216,8 @@ int main(int argc, char **argv)
 
     argind = 1;
 
+    vec_init(&startup_cmd, sizeof(vec));
+
     while (argind < argc)
         process_arg(argc, argv, &argind);
 
@@ -179,6 +236,8 @@ int main(int argc, char **argv)
 
     w.pri = (cur){0, 14};
     w.sec = (cur){0, 14};
+
+    run_startup_cmd();
 
     win_out_after(&w, (cur){0, 0});
     win_out_bar(&w);
