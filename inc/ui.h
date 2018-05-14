346258dc1ba71c3496897b7e93d6a73bfabe00bc
Xeom <fwharf.fw@gmail.com>
2018-05-15 00:32:23 +0100

    Replaced ui.c with bind

--- inc/ui.h
+++ inc/ui.h
@@ -4,31 +4,9 @@
 # include "vec.h"
 # include "win.h"
 
-typedef enum
-{
-    ui_mode_buf,
-    ui_mode_kcd,
-    ui_mode_bar
-} ui_mode_type;
-
 extern int ui_alive;
-extern ui_mode_type ui_mode;
+
 
 void ui_cmd_cb(win *w, vec *cmd);
 
-void ui_init(void);
-
-void ui_kill(void);
-
-int ui_is_typable(inp_key key);
-
-void ui_ins_flush(void);
-
-void ui_flush(void);
-
-void ui_ins(inp_key key);
-
-void ui_handle(inp_key key);
-
-void ui_activate_cmd(void);
 #endif
