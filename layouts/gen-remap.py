e275868c1a07e598055826b3b64ec8c6ff91935e
Xeom <fwharf.fw@gmail.com>
2018-06-06 18:58:45 +0100

    Added error checking for invalid remapped ctrl keys
     * These can't really be fixed automatically, so we'll need to
       do so manually when these configs are used.

--- layouts/gen-remap.py
+++ layouts/gen-remap.py
@@ -2,22 +2,10 @@
 
 import sys
 
-if len(sys.argv) != 3:
-    print("gen-remap.py [from] [to]")
-    exit()
+ctrlable = "@ABCDEFGKLNOPQRSTUVWXYZ]^_"
 
-try:
-    fromfile = open(sys.argv[1], "r")
-    tofile   = open(sys.argv[2], "r")
-
-except IOError as e:
-    print(e)
-    exit()
-
-fromcodes = dict(l.strip().split()[::-1] for l in fromfile.readlines())
-tocodes   = dict(l.strip().split()[::-1] for l in tofile.readlines())
-
-remaps = {key : tocodes.get(code, None) for code, key in fromcodes.items()}
+def err(s):
+    print(s, file=sys.stderr)
 
 def convert(code):
     mods =     code & 0xff00
@@ -26,11 +14,28 @@ def convert(code):
         remap = remaps[c]
 
         if remap == None:
-            print("Could not remap '{}'".format(c))
+            err("Could not remap '{}'".format(c))
             return None
 
         else:
             return ord(remaps[c]) | mods
+
+if len(sys.argv) != 3:
+    err("gen-remap.py [from] [to]")
+    exit()
+
+try:
+    fromfile = open(sys.argv[1], "r")
+    tofile   = open(sys.argv[2], "r")
+
+except IOError as e:
+    err(e)
+    exit()
+
+fromcodes = dict(l.strip().split()[::-1] for l in fromfile.readlines())
+tocodes   = dict(l.strip().split()[::-1] for l in tofile.readlines())
+
+remaps = {key : tocodes.get(code, None) for code, key in fromcodes.items()}
 
 for line in sys.stdin.readlines():
     if not line.startswith(" "):
@@ -41,7 +46,19 @@ for line in sys.stdin.readlines():
     code = int(code, 16)
     conv = convert(code)
 
-    if (conv != None):
+    if (conv == None): continue
+
+    c    = chr(conv & 0xff)
+
+    if (conv & 0x400 and c not in ctrlable):
+        err("{} is getting mapped to a key involving Ctrl+{}".format(bind, c))
+        print("\n# The following is an invalid keybinding. Please remap this manually.")
+        print("# the original keybinding on qwerty was {:x}".format(code))
+        print("# unmap {} {:x}".format(mode, code))
+        print("# remap {} {:x} {}\n".format(mode, conv, bind))
+
+    else:
+        print("unmap {} {:x}".format(mode, code))
         print("remap {} {:x} {}".format(mode, conv, bind))
 
 exit(0)
