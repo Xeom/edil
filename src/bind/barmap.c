3888ad858e4ef5fb709fd85fd53e62c7446bfc6a
Xeom <fwharf.fw@gmail.com>
2018-06-15 22:40:42 +0100

    Added end and home bindings to bar mode
     * I cheated and just did move(1000) and (-1000)

--- src/bind/barmap.c
+++ src/bind/barmap.c
@@ -12,9 +12,13 @@ void bind_barmap_init()
     BIND_MAP(bar, bar_back,  inp_key_back);
     BIND_MAP(bar, bar_del,   inp_key_del);
 
+    BIND_MAP(bar, bar_home, inp_key_home);
+    BIND_MAP(bar, bar_end,  inp_key_end);
+
     BIND_MAP(bar, mode_buf,  inp_key_ctrl | 'X');
-    BIND_MAP(bar, mode_buf,  inp_key_ctrl | 'A');
-    BIND_MAP(bar, mode_kcd,  inp_key_ctrl | 'K');
+    BIND_MAP(bar, mode_buf,  inp_key_ctrl | 'E');
+    BIND_MAP(bar, mode_lng,  inp_key_ctrl | 'L')
+    BIND_MAP(bar, mode_kcd,  inp_key_ctrl | '^');
 }
 
 void bind_bar_ins(win *w, vec *text)
